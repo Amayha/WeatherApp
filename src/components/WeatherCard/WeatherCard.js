e1a69bf447deaef97cc6ad9f9055267a6f788e21
Pau <paula_ale_pe@hotmail.com>
2019-09-05 16:23:13 -0500

    se conecta con el API

--- src/components/WeatherCard/WeatherCard.js
+++ src/components/WeatherCard/WeatherCard.js
@@ -1,38 +1,97 @@
 import React from 'react';
-import {Card, CardContent, makeStyles} from '@material-ui/core';
+import { Card, CardContent, makeStyles } from '@material-ui/core';
+
 
 
 
 function WeatherCard(props) {
     const classes = useStyles();
+    var today;
 
-  return ( <Card>
-      <CardContent className={classes.content}>
-          <h3 className= {classes.title}>{props.day}</h3>
-          <img src={props.icon} alt=''/>
-          <p className={classes.info}>
-             <span>{props.min}</span> {props.max}
-          </p>
+    switch (props.dayI) {
 
-      </CardContent>
-  </Card>
+        case 1:
+            today = 'Mon'
+            break;
 
-  );
+        case 2:
+            today = 'Tue'
+            break;
+
+        case 3:
+            today = 'Wed'
+            break;
+
+        case 4:
+            today = 'Tru'
+            break;
+
+        case 5:
+            today = 'Fri'
+            break;
+
+        case 6:
+            today = 'Sat'
+            break;
+
+        case 7:
+            today = 'Sun'
+            break;
+
+    }
+
+    if (today === props.day)
+        return (<Card>
+            <CardContent className={classes.content}>
+                <h3 className={classes.titleB}>{props.day}</h3>
+                <img src={props.icon} alt='' />
+                <p className={classes.info}>
+                    <span>{props.min}</span> {props.max}
+                </p>
+            </CardContent>
+        </Card>
+        );
+    else
+        return (<Card>
+            <CardContent className={classes.content}>
+                <h3 className={classes.title}>{props.day}</h3>
+                <img src={props.icon} alt='' />
+                <p className={classes.info}>
+                    <span>{props.min}</span> {props.max}
+                </p>
+            </CardContent>
+        </Card>
+        );
+
+
 }
 
 const useStyles = makeStyles(theme => ({
-    title:{
+    title: {
         fontSize: 25,
         margin: 0,
     },
-    img:{
+    titleB: {
+        fontSize: 25,
+        margin: 0,
+        color: 'red',
 
     },
-    content:{
+    img: {
+
+    },
+    content: {
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    contentB: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+       // margin: 20,
     },
     info: {
         margin: 0,
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,74 +1,132 @@
 import React from 'react';
 import WeatherCard from '../../components/WeatherCard/WeatherCard'
-import { Grid, CssBaseline} from '@material-ui/core';
+import { Grid, CssBaseline, Button } from '@material-ui/core';
+import moment from 'moment';
+import 'moment/locale/es';
 
+moment.locale('es');
+
+var myDate = new Date(),
+  dayI = myDate.getDay();
+console.log(myDate);
+console.log(dayI);
+
+
+
+/*
 const days= [
   {
     day : 'Wed',
     icon: "https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png",
     min: '70°',
     max:'90°',
+    dayI,
   },
   {
     day : 'Tru',
     icon: "https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png",
     min: '70°',
     max:'90°',
+    dayI,
   },
   {
     day : 'Fri',
     icon: "https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png",
     min: '70°',
     max:'90°',
+    dayI,
   },
   {
     day : 'Sat',
     icon: "https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png",
     min: '70°',
     max:'90°',
+    dayI,
   },
   {
     day : 'Sun',
     icon: "https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png",
     min: '70°',
     max:'90°',
+    dayI,
   },
   {
     day : 'Mon',
     icon: "https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png",
     min: '70°',
     max:'90°',
+    dayI,
   },
   {
     day : 'Tue',
     icon: "https://ssl.gstatic.com/onebox/weather/48/partly_cloudy.png",
     min: '70°',
     max:'90°',
+    dayI,
   },
-]
+]*/
+
 
 function App() {
+
+  const [days, setDays,] = React.useState([]);
+
+  const handleGetWeather = () => {
+    console.log('get weather');
+    var promise = fetch('http://api.openweathermap.org/data/2.5/forecast?q=Cali,co&APPID=79596a3b0efba5c91d3af2f7826d7a07');
+
+    promise.then((info) => {
+      console.log(info);
+      return info.json();
+    })
+      .then((info) => {
+        var list = info.list.filter((elem, index) => {
+          return index % 8 === 0; // filtramos por multiplos de 8 porque cada 8 items de la lista cambia de dia
+        });
+
+        list = list.map((elem, index, array) => {
+          return {
+            day: moment.unix(elem.dt).format('ddd [/] D [/] MM [/]YYYY'),
+            icon: `https://openweathermap.org/img/wn/${elem.weather[0].icon}.png`,
+            min: Math.round( elem.main.temp_min -273.15) + '°',
+            max:  Math.round( elem.main.temp_max -273.15) + '°',
+          }
+
+        });
+
+        
+
+        setDays(list);
+        console.log(days);
+      });
+  }
+  // un if, cuando days sea true se acciona 'days &&'
   return (
     <div>
-      
+
+      <Button onClick={handleGetWeather} variant="contained" color="primary">
+        Get weather Info
+      </Button>
+
       <Grid container >
 
-        {days.map((item)=>  <Grid item md={1}>
+        {days && days.map((item) => <Grid item md={3} key={item.day}>
           <WeatherCard
-          day = {item.day}
-          icon= {item.icon}
-          min= {item.min}
-          max= {item.max}
+            day={item.day}
+            icon={item.icon}
+            min={item.min}
+            max={item.max}
+            dayI={item.dayI}
           />
-          </Grid>)}
+        </Grid>)}
 
-         
+
 
       </Grid>
 
-      <CssBaseline/>
+      <CssBaseline />
     </div>
-   
+
   );
 }
 
